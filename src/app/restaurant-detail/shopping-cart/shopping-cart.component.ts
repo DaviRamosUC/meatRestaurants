7163e2ba2d7e4e8d350fd37dff498904ab6a63f4
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-07-28 21:21:36 -0300

    finalizando carrinho de compras

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../menu-item/menu-item.module';
 import { CartItem } from './cart-item.module';
 
 import { ShoppingCartService } from './shopping-cart.service';
@@ -24,6 +25,14 @@ export class ShoppingCartComponent implements OnInit {
 
   clear(): void {
     this.shoppingCartService.clear()
+  }
+
+  removeItem(item: CartItem): void {
+    this.shoppingCartService.removeItem(item);
+  }
+
+  addItem(item: MenuItem): void {
+    this.shoppingCartService.addItem(item);
   }
 
 }
